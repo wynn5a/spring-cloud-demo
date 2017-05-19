ab89bc8ce472413e20e5a7c43d03ba0027b06fb8
Wayne <fuwenming1990@gmail.com>
2017-05-19 11:49:29 +0800

    Initial commit

--- feign-demo/src/main/java/io/github/wynn5a/service/CityService.java
+++ feign-demo/src/main/java/io/github/wynn5a/service/CityService.java
@@ -2,14 +2,39 @@ package io.github.wynn5a.service;
 
 import io.github.wynn5a.dto.City;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
+ * add hystrix for fault tolerance
+ * <p>
  * Created by fuwenming on 2017/4/19.
  */
-@FeignClient(name = "service-provider-demo")
+@FeignClient(name = "service-provider-demo", fallback = CityService.HystrixDemo.class)
 public interface CityService {
     @RequestMapping("cities/{id}")
     City findById(@RequestParam("id") Long id);
+
+    @Component
+    class HystrixDemo implements CityService {
+        private City failCity = null;
+
+        @Override
+        public City findById(Long id) {
+            return getFailCity();
+        }
+
+        private City getFailCity() {
+            if (failCity == null) {
+                failCity = new City();
+                failCity.setCountry("UNKNOWN COUNTRY");
+                failCity.setId(-1l);
+                failCity.setName("UNKNOWN NAME");
+                failCity.setState("UNKNOWN STATE");
+            }
+
+            return failCity;
+        }
+    }
 }
